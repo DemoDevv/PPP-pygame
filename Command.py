7c7791919be02c95f99a6b8f6c0f5df0445bb58c
DemoDevv <mathieulebras95@gmail.com>
2023-05-04 19:45:35 +0200

    V1.3.6 (ajout de commandes)

--- Command.py
+++ Command.py
@@ -1,6 +1,16 @@
+import sys
+
+
 class Command:
+    """ Classe qui représente une commande que l'on peut utiliser dans le jeu """
 
     def __init__(self, name, description, function):
         self.name = name
         self.description = description
         self.function = function
+
+
+commands = [
+    Command("help", "Affiche la liste des commandes", lambda: print("\n".join([f"{command.name} : {command.description}" for command in commands]))),
+    Command("quit", "Quitte le jeu", lambda: sys.exit()),
+]
