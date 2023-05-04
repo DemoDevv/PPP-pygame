d4475e4b9b0acd72f6a018d4e9426b1fa16dad91
E225868J <E225868J>
2023-05-04 09:06:38 +0200

    V1.2.3 (+fichier dependances python)

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from Game import Game
 if __name__ == "__main__":
     pygame.init()
     game = Game()
-    game.screen = pygame.display.set_mode(game.get_screen_dimensions(), vsync=1)
+    game.screen = pygame.display.set_mode(game.get_screen_dimensions())
     game.clock = pygame.time.Clock()
     game.init()
 
